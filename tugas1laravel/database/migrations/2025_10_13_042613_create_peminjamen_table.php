e81bd8222586d9a832e282abfaf54b90ec77189e
shenoraaa <deviaspire2@gmail.com>
2025-10-13 11:41:54 +0700

    menambahkan model dan migration -Devi

---
+++ tugas1laravel/database/migrations/2025_10_13_042613_create_peminjamen_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('peminjaman', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_peminjam');
+            $table->string('judul_buku');
+            $table->date('tanggal_pinjam');
+            $table->date('tanggal_kembali')->nullable();
+            $table->string('status')->default('dipinjam'); // contoh: dipinjam, dikembalikan
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('peminjaman');
+    }
+};
